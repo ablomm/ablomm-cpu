9af60e6a63eeb46810591acdf663c7fb4959fbf6
Andrew Blommestyn <71096624+ablomm@users.noreply.github.com>
2024-07-27 19:55:46 -0400

    Redo ast

--- assembler/src/parser.rs
+++ assembler/src/parser.rs
@@ -1,32 +1,8 @@
+use chumsky::prelude::*;
 use std::char;
 
-use chumsky::prelude::*;
-
-#[derive(Debug)]
-pub enum Token {
-    Label(String),
-    Number(u32),
-    Register(String),
-
-    Mnemonic(String),
-    Modifier(String),
-    FullMnemonic {
-        mnemonic: Box<Token>,
-        modifiers: Vec<Token>,
-    },
-
-    Parameter(Box<Token>),
-
-    Operation {
-        full_mnemonic: Box<Token>,
-        parameters: Vec<Token>,
-    },
-
-    Assembly(Vec<Token>),
-}
-
-#[derive(Debug)]
-enum Reg {
+#[derive(Debug, Clone)]
+pub enum Register {
     R0,
     R1,
     R2,
@@ -45,8 +21,8 @@ enum Reg {
     PC,
 }
 
-#[derive(Debug)]
-enum Mnemonic {
+#[derive(Debug, Clone)]
+pub enum Mnemonic {
     And,
     Or,
     Xor,
@@ -56,61 +32,171 @@ enum Mnemonic {
     Sub,
     Subb,
     Shl,
-    Ashl,
     Shr,
+    Ashr,
     Ld,
     St,
     Push,
     Pop,
+    Int,
+}
+
+#[derive(Debug, Clone)]
+pub enum Condition {
+    Eq,
+    Ne,
+    Ltu,
+    Gtu,
+    Leu,
+    Geu,
+    Lts,
+    Gts,
+    Les,
+    Ges,
+}
+
+#[derive(Debug, Clone)]
+pub enum AluModifier {
+    S,
+    T,
+}
+
+#[derive(Debug, Clone)]
+pub enum Modifier {
+    Condition(Condition),
+    AluModifier(AluModifier),
 }
 
 #[derive(Debug)]
-enum Modifier {}
+pub enum Parameter {
+    Label(String),
+    Number(u32),
+    Register(Register),
+}
 
-pub fn parser() -> impl Parser<char, Token, Error = Simple<char>> {
-    let label = text::ident()
-        .then_ignore(just(':'))
-        .map(|s: String| Token::Label(s));
+#[derive(Debug)]
+pub struct FullMnemonic {
+    mnemonic: Box<Mnemonic>,
+    modifiers: Vec<Modifier>,
+}
+
+#[derive(Debug)]
+pub struct Operation {
+    full_mnemonic: Box<FullMnemonic>,
+    parameters: Vec<Parameter>,
+}
+
+#[derive(Debug)]
+pub enum Statement {
+    Operation(Box<Operation>),
+    Label(String),
+}
+
+pub fn parser() -> impl Parser<char, Vec<Statement>, Error = Simple<char>> {
+    let label = text::ident();
 
     let hex_num = just("#0x")
         .ignore_then(text::int(16).map(|s: String| u32::from_str_radix(&s, 16).unwrap()));
     let dec_num =
         just("#").ignore_then(text::int(10).map(|s: String| u32::from_str_radix(&s, 10).unwrap()));
 
-    let number = hex_num
-        .clone()
-        .or(dec_num.clone())
-        .padded()
-        .map(|n: u32| Token::Number(n));
+    let number = hex_num.clone().or(dec_num.clone());
 
-    let mnemonic = text::ident().padded().map(|s: String| Token::Mnemonic(s));
-    let register = text::ident().padded().map(|s: String| Token::Register(s));
+    let mnemonic = choice((
+        text::keyword("and").to(Mnemonic::And),
+        text::keyword("or").to(Mnemonic::Or),
+        text::keyword("xor").to(Mnemonic::Xor),
+        text::keyword("not").to(Mnemonic::Not),
+        text::keyword("add").to(Mnemonic::Add),
+        text::keyword("addc").to(Mnemonic::Addc),
+        text::keyword("sub").to(Mnemonic::Sub),
+        text::keyword("subb").to(Mnemonic::Subb),
+        text::keyword("shl").to(Mnemonic::Shl),
+        text::keyword("shr").to(Mnemonic::Shr),
+        text::keyword("ashr").to(Mnemonic::Ashr),
+        text::keyword("ld").to(Mnemonic::Ld),
+        text::keyword("st").to(Mnemonic::St),
+        text::keyword("push").to(Mnemonic::Push),
+        text::keyword("pop").to(Mnemonic::Pop),
+        text::keyword("int").to(Mnemonic::Int),
+    ));
 
-    let modifier = just('.').ignore_then(text::ident().map(|s: String| Token::Modifier(s)));
+    let register = choice((
+        text::keyword("r0").to(Register::R0),
+        text::keyword("r1").to(Register::R1),
+        text::keyword("r2").to(Register::R2),
+        text::keyword("r3").to(Register::R3),
+        text::keyword("r4").to(Register::R4),
+        text::keyword("r5").to(Register::R5),
+        text::keyword("r6").to(Register::R6),
+        text::keyword("r7").to(Register::R7),
+        text::keyword("r8").to(Register::R8),
+        text::keyword("r9").to(Register::R9),
+        text::keyword("r10").to(Register::R10),
+        text::keyword("r11").to(Register::R11),
+        text::keyword("fp").to(Register::FP),
+        text::keyword("status").to(Register::STATUS),
+        text::keyword("sp").to(Register::SP),
+        text::keyword("pc").to(Register::PC),
+    ));
+
+    let alu_modifier = choice((
+        text::keyword("s").to(AluModifier::S),
+        text::keyword("T").to(AluModifier::T),
+    ));
+
+    let condition = choice((
+        text::keyword("eq").to(Condition::Eq),
+        text::keyword("ne").to(Condition::Ne),
+        text::keyword("ltu").to(Condition::Ltu),
+        text::keyword("gtu").to(Condition::Gtu),
+        text::keyword("leu").to(Condition::Leu),
+        text::keyword("geu").to(Condition::Geu),
+        text::keyword("lts").to(Condition::Lts),
+        text::keyword("gts").to(Condition::Gts),
+        text::keyword("les").to(Condition::Les),
+        text::keyword("ges").to(Condition::Ges),
+    ));
+
+    let modifier = just('.').ignore_then(
+        alu_modifier
+            .clone()
+            .map(|am| Modifier::AluModifier(am))
+            .or(condition.clone().map(|c| Modifier::Condition(c))),
+    );
 
     let full_mnemonic =
         mnemonic
             .clone()
             .then(modifier.clone().repeated())
-            .map(|(mnemonic, modifiers)| Token::FullMnemonic {
+            .map(|(mnemonic, modifiers)| FullMnemonic {
                 mnemonic: Box::new(mnemonic),
                 modifiers,
             });
 
-    let parameter = number.clone().or(register.clone());
+    let parameter = number
+        .clone()
+        .map(|n| Parameter::Number(n))
+        .or(register.clone().map(|r| Parameter::Register(r)));
 
     let operation = full_mnemonic
         .clone()
-        .then(parameter.clone().separated_by(just(',')))
-        .then_ignore(just(';'))
-        .map(|(full_mnemonic, parameters)| Token::Operation {
+        .padded()
+        .then(parameter.clone().padded().separated_by(just(',')))
+        .map(|(full_mnemonic, parameters)| Operation {
             full_mnemonic: Box::new(full_mnemonic),
             parameters,
         });
 
-    let assembly = (operation.clone().or(label.clone()).padded())
-        .repeated()
-        .map(|operations| Token::Assembly(operations));
+    let statement = operation
+        .clone()
+        .then_ignore(just(';'))
+        .map(|o| Statement::Operation(Box::new(o)))
+        .or(label
+            .clone()
+            .then_ignore(just(':'))
+            .map(|l| Statement::Label(l)))
+        .padded();
 
-    return assembly.then_ignore(end());
+    return statement.repeated().then_ignore(end());
 }
