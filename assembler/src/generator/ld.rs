91808e4ab83c3668c4be858a4c661196d8005fa6
ablomm <71096624+ablomm@users.noreply.github.com>
2024-11-09 20:58:57 -0500

    code readability

--- assembler/src/generator/ld.rs
+++ assembler/src/generator/ld.rs
@@ -12,61 +12,110 @@ pub fn generate_ld(
     let mut opcode: u32 = 0;
     opcode |= operation.full_mnemonic.modifiers.generate() & (0b1111 << 28);
 
-    if let Parameter::Register(register, _) = operation.parameters[0] {
-        if let Parameter::Indirect(parameter, _) = &operation.parameters[1] {
-            if let Parameter::Register(register2, _) = **parameter {
-                //LDR
-                opcode |= Mnemonic::LDR.generate();
-                opcode |= register.generate() << 16;
-                opcode |= register2.generate() << 12;
-                return Ok(opcode);
-            } else if let Parameter::Number(number, _) = **parameter {
-                //LD
-                opcode |= Mnemonic::LD.generate();
-                opcode |= register.generate() << 16;
-                opcode |= number & 0xffff;
-                return Ok(opcode);
-            } else if let Parameter::Label(label, _) = &**parameter {
-                //LD
-                opcode |= Mnemonic::LD.generate();
-                opcode |= register.generate() << 16;
-                if let Some(label_line) = symbol_table.get(&*label) {
-                    opcode |= label_line & 0xffff;
-                    return Ok(opcode);
-                } else {
-                    return Err("Could not find label in LD");
-                }
-            } else {
-                return Err("LD only supports indirect constants, registers, and labels");
-            }
-        } else if let Parameter::Register(register2, _) = operation.parameters[1] {
-            //MOV
-            opcode |= Mnemonic::PASSA.generate();
-            opcode |= register.generate() << 12;
-            opcode |= register2.generate() << 8;
-            return Ok(opcode);
-        } else if let Parameter::Number(number, _) = operation.parameters[1] {
-            //LDI
-            opcode |= Mnemonic::LDI.generate();
-            opcode |= register.generate() << 16;
-            opcode |= number & 0xffff;
-            return Ok(opcode);
-        } else if let Parameter::Label(label, _) = &operation.parameters[1] {
-            //LDI
-            opcode |= Mnemonic::LDI.generate();
-            opcode |= register.generate() << 16;
-            if let Some(label_line) = symbol_table.get(&*label) {
-                opcode |= label_line & 0xffff;
-                return Ok(opcode);
-            } else {
-                return Err("Could not find label in LD");
-            }
-        } else {
-            return Err(
-                "Expected second LD parameter to be either an indirect, register, or number",
-            );
+    match &operation.parameters[0] {
+        Parameter::Register(register, _) => {
+            return generate_ld_reg(register, opcode, operation, symbol_table)
         }
+
+        _ => return Err("Expected first LD parameter to be a register"),
+    }
+}
+
+fn generate_ld_reg(
+    register: &Register,
+    mut opcode: u32,
+    operation: &Operation,
+    symbol_table: &HashMap<String, u32>,
+) -> Result<u32, &'static str> {
+    opcode |= register.generate() << 16;
+
+    match &operation.parameters[1] {
+        Parameter::Register(register2, _) => {
+            return generate_ld_reg_reg(register, &register2, opcode)
+        }
+
+        Parameter::Number(number, _) => return generate_ld_reg_num(*number, opcode),
+        Parameter::Label(label, _) => return generate_ld_reg_label(label, opcode, symbol_table),
+        Parameter::Indirect(parameter, _) => {
+            return generate_ld_reg_indirect(parameter, opcode, symbol_table)
+        }
+    }
+}
+
+fn generate_ld_reg_reg(
+    register: &Register,
+    register2: &Register,
+    mut opcode: u32,
+) -> Result<u32, &'static str> {
+    // MOV
+    // this is really just an alu_op
+    opcode |= Mnemonic::PASSA.generate();
+    opcode &= !(0b1111 << 16); // need to zero out previously set register
+    opcode |= register.generate() << 12;
+    opcode |= register2.generate() << 8;
+    return Ok(opcode);
+}
+
+fn generate_ld_reg_num(number: u32, mut opcode: u32) -> Result<u32, &'static str> {
+    // LDI
+    opcode |= Mnemonic::LDI.generate();
+    opcode |= number & 0xffff;
+    return Ok(opcode);
+}
+
+fn generate_ld_reg_label(
+    label: &str,
+    mut opcode: u32,
+    symbol_table: &HashMap<String, u32>,
+) -> Result<u32, &'static str> {
+    // LDI
+    opcode |= Mnemonic::LDI.generate();
+    if let Some(label_line) = symbol_table.get(label) {
+        opcode |= label_line & 0xffff;
+        return Ok(opcode);
     } else {
-        return Err("Expected first LD parameter to be a register");
+        return Err("Could not find label in LD");
+    }
+}
+
+fn generate_ld_reg_indirect(
+    parameter: &Parameter,
+    opcode: u32,
+    symbol_table: &HashMap<String, u32>,
+) -> Result<u32, &'static str> {
+    match parameter {
+        Parameter::Register(register2, _) => return generate_ld_reg_ireg(register2, opcode),
+        Parameter::Number(number, _) => return generate_ld_reg_inum(*number, opcode),
+        Parameter::Label(label, _) => return generate_ld_reg_ilabel(label, opcode, symbol_table),
+        _ => return Err("LD only supports indirect constants, registers, and labels"),
+    }
+}
+// indirect register
+fn generate_ld_reg_ireg(register2: &Register, mut opcode: u32) -> Result<u32, &'static str> {
+    // LDR
+    opcode |= Mnemonic::LDR.generate();
+    opcode |= register2.generate() << 12;
+    return Ok(opcode);
+}
+
+fn generate_ld_reg_inum(number: u32, mut opcode: u32) -> Result<u32, &'static str> {
+    // LD
+    opcode |= Mnemonic::LD.generate();
+    opcode |= number & 0xffff;
+    return Ok(opcode);
+}
+
+fn generate_ld_reg_ilabel(
+    label: &str,
+    mut opcode: u32,
+    symbol_table: &HashMap<String, u32>,
+) -> Result<u32, &'static str> {
+    // LD
+    opcode |= Mnemonic::LD.generate();
+    if let Some(label_line) = symbol_table.get(label) {
+        opcode |= label_line & 0xffff;
+        return Ok(opcode);
+    } else {
+        return Err("Could not find label in LD");
     }
 }
--- assembler/src/generator/st.rs
+++ assembler/src/generator/st.rs
@@ -12,43 +12,86 @@ pub fn generate_st(
     let mut opcode: u32 = 0;
     opcode |= operation.full_mnemonic.modifiers.generate() & (0b1111 << 28);
 
-    if let Parameter::Register(register, _) = operation.parameters[0] {
-        if let Parameter::Indirect(parameter, _) = &operation.parameters[1] {
-            if let Parameter::Register(register2, _) = **parameter {
-                // STR
-                opcode |= Mnemonic::STR.generate();
-                opcode |= register.generate() << 16;
-                opcode |= register2.generate() << 12;
-                return Ok(opcode);
-            } else if let Parameter::Number(number, _) = **parameter {
-                // ST
-                opcode |= Mnemonic::ST.generate();
-                opcode |= register.generate() << 16;
-                opcode |= number & 0xffff;
-                return Ok(opcode);
-            } else if let Parameter::Label(label, _) = &**parameter {
-                // ST
-                opcode |= Mnemonic::ST.generate();
-                opcode |= register.generate() << 16;
-                if let Some(label_line) = symbol_table.get(&*label) {
-                    opcode |= label_line & 0xffff;
-                    return Ok(opcode);
-                } else {
-                    return Err("Could not find label in ST");
-                }
-            } else {
-                return Err("ST only supports indirect constants, registers, and labels");
-            }
-        } else if let Parameter::Register(register2, _) = operation.parameters[1] {
-            // MOVR
-            opcode |= Mnemonic::PASSA.generate();
-            opcode |= register2.generate() << 12;
-            opcode |= register.generate() << 8;
-            return Ok(opcode);
-        } else {
-            return Err("Expected second ST parameter to be either an indirect or register");
+    match &operation.parameters[0] {
+        Parameter::Register(register, _) => {
+            return generate_st_reg(register, opcode, operation, symbol_table)
         }
+
+        _ => return Err("Expected first LD parameter to be a register"),
+    }
+}
+
+fn generate_st_reg(
+    register: &Register,
+    mut opcode: u32,
+    operation: &Operation,
+    symbol_table: &HashMap<String, u32>,
+) -> Result<u32, &'static str> {
+    opcode |= register.generate() << 16;
+
+    match &operation.parameters[1] {
+        Parameter::Register(register2, _) => {
+            return generate_st_reg_reg(register, &register2, opcode)
+        }
+        Parameter::Indirect(parameter, _) => {
+            return generate_st_reg_indirect(parameter, opcode, symbol_table)
+        }
+
+        _ => return Err("Expected second ST parameter to be either an indirect or register"),
+    }
+}
+
+fn generate_st_reg_reg(
+    register: &Register,
+    register2: &Register,
+    mut opcode: u32,
+) -> Result<u32, &'static str> {
+    // MOVR
+    opcode |= Mnemonic::PASSA.generate();
+    opcode &= !(0b1111 << 16); // need to zero out previously set register
+    opcode |= register2.generate() << 12;
+    opcode |= register.generate() << 8;
+    return Ok(opcode);
+}
+
+fn generate_st_reg_indirect(
+    parameter: &Parameter,
+    opcode: u32,
+    symbol_table: &HashMap<String, u32>,
+) -> Result<u32, &'static str> {
+    match parameter {
+        Parameter::Register(register2, _) => return generate_st_reg_ireg(register2, opcode),
+        Parameter::Number(number, _) => return generate_st_reg_inum(*number, opcode),
+        Parameter::Label(label, _) => return generate_st_reg_ilabel(label, opcode, symbol_table),
+        _ => return Err("ST only supports indirect constants, registers, and labels"),
+    }
+}
+
+fn generate_st_reg_ireg(register2: &Register, mut opcode: u32) -> Result<u32, &'static str> {
+    // STR
+    opcode |= Mnemonic::STR.generate();
+    opcode |= register2.generate() << 12;
+    return Ok(opcode);
+}
+
+fn generate_st_reg_inum(number: u32, mut opcode: u32) -> Result<u32, &'static str> {
+    // ST
+    opcode |= Mnemonic::ST.generate();
+    opcode |= number & 0xffff;
+    return Ok(opcode);
+}
+
+fn generate_st_reg_ilabel(
+    label: &str,
+    mut opcode: u32,
+    symbol_table: &HashMap<String, u32>,
+) -> Result<u32, &'static str> {
+    // ST
+    opcode |= Mnemonic::ST.generate();
+    if let Some(label_line) = symbol_table.get(label) {
+        opcode |= label_line & 0xffff;
+        return Ok(opcode);
     } else {
-        return Err("Expected first LD parameter to be a register");
+        return Err("Could not find label in ST");
     }
 }
