161b190c4c88de855248a5c326d6daf2889bea75
Andrew Blommestyn <71096624+ablomm@users.noreply.github.com>
2024-11-13 00:58:44 -0500

    Simplify error handling

--- assembler/src/lib.rs
+++ assembler/src/lib.rs
@@ -15,19 +15,13 @@ pub fn assemble(assembly_code: &str, src: Intern<String>) -> Result<String, Vec<
     let len = assembly_code.chars().count();
     let eoi = Span::new(src, len..len);
 
-    match parser().parse(chumsky::Stream::from_iter(
+    let ast = parser().parse(chumsky::Stream::from_iter(
         eoi,
         assembly_code
             .chars()
             .enumerate()
             .map(|(i, c)| (c, Span::new(src, i..i + 1))),
-    )) {
-        Ok(ast) => match generate(ast) {
-            Ok(machine_code) => Ok(machine_code),
-            Err(error) => return Err(vec![error]),
-        },
-        Err(error) => {
-            return Err(error);
-        }
-    }
+    ))?;
+
+    return generate(ast).map_err(|error| vec![error]);
 }
