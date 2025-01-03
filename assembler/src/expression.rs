594c5b2770853848d04110b25bf5e2eb13eb885f
Andrew Blommestyn <71096624+ablomm@users.noreply.github.com>
2025-01-03 16:55:47 -0500

    Macro make expression eval simpler

--- assembler/src/expression.rs
+++ assembler/src/expression.rs
@@ -12,6 +12,16 @@ use crate::{
 
 pub mod expression_result;
 
+macro_rules! op {
+    ($e:expr, $symbol_table:ident, $waiting_map:ident, $($val:ident),* ) => {{
+        $(
+            let $val = get_operand($val, $symbol_table, &mut $waiting_map)?;
+            let $val = &$val.as_ref();
+        )*
+        $e
+    }};
+}
+
 pub struct EvalReturn {
     pub result: ExpressionResult,
     // the identifiers that are causing the result to be None. It is empty if the value is known
@@ -23,10 +33,6 @@ impl Spanned<&Expression> {
     pub fn eval(&self, symbol_table: &SymbolTable) -> Result<EvalReturn, Error> {
         let mut waiting_map = HashMap::new();
         let result = match self.val {
-            // there is a bunch of deref's here (i.e. **a) because a and b are a Box, which has
-            // it's own as_ref() function, but we really need the Spanned::as_ref() function. No
-            // deref's are needed if the Spanned::as_ref() method is named differently, but I
-            // didn't like that
             Expression::Register(reg) => ExpressionResult::Register(Some(*reg)),
             Expression::String(string) => ExpressionResult::String(Some(String(string.clone()))),
             Expression::Number(a) => ExpressionResult::Number(Some(Number(*a))),
@@ -44,207 +50,21 @@ impl Spanned<&Expression> {
                 }
                 get_identifier(&self.span_to(a), symbol_table)?.result.val
             }
-            Expression::Ref(a) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                a.as_ref().asm_ref()?
-            }
-            Expression::Deref(a) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                a.as_ref().asm_deref()?
-            }
-            Expression::Neg(a) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (-&a.as_ref())?
-            }
-            Expression::Not(a) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (!&a.as_ref())?
-            }
-            Expression::Mul(a, b) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**b).as_ref().eval(symbol_table)?;
-                let b = b.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (&a.as_ref() * &b.as_ref())?
-            }
-            Expression::Div(a, b) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**b).as_ref().eval(symbol_table)?;
-                let b = b.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (&a.as_ref() / &b.as_ref())?
-            }
-            Expression::Remainder(a, b) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**b).as_ref().eval(symbol_table)?;
-                let b = b.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (&a.as_ref() % &b.as_ref())?
-            }
-            Expression::Add(a, b) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**b).as_ref().eval(symbol_table)?;
-                let b = b.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (&a.as_ref() + &b.as_ref())?
-            }
-            Expression::Sub(a, b) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**b).as_ref().eval(symbol_table)?;
-                let b = b.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (&a.as_ref() - &b.as_ref())?
-            }
-            Expression::Shl(a, b) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**b).as_ref().eval(symbol_table)?;
-                let b = b.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (&a.as_ref() << &b.as_ref())?
-            }
-            Expression::Shr(a, b) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**b).as_ref().eval(symbol_table)?;
-                let b = b.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (&a.as_ref() >> &b.as_ref())?
-            }
-            Expression::Ashr(a, b) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**b).as_ref().eval(symbol_table)?;
-                let b = b.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (a.as_ref().ashr(&b.as_ref()))?
-            }
-            Expression::And(a, b) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**b).as_ref().eval(symbol_table)?;
-                let b = b.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (&a.as_ref() & &b.as_ref())?
-            }
-            Expression::Or(a, b) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**b).as_ref().eval(symbol_table)?;
-                let b = b.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (&a.as_ref() | &b.as_ref())?
-            }
-            Expression::Xor(a, b) => {
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**a).as_ref().eval(symbol_table)?;
-                let a = a.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                let EvalReturn {
-                    result,
-                    waiting_map: sub_waiting_map,
-                } = (**b).as_ref().eval(symbol_table)?;
-                let b = b.span_to(result);
-                waiting_map.extend(sub_waiting_map.iter());
-                (&a.as_ref() ^ &b.as_ref())?
-            }
+            Expression::Ref(a) => op!(a.asm_ref(), symbol_table, waiting_map, a)?,
+            Expression::Deref(a) => op!(a.asm_deref(), symbol_table, waiting_map, a)?,
+            Expression::Neg(a) => op!(-a, symbol_table, waiting_map, a)?,
+            Expression::Not(a) => op!(!a, symbol_table, waiting_map, a)?,
+            Expression::Mul(a, b) => op!(a * b, symbol_table, waiting_map, a, b)?,
+            Expression::Div(a, b) => op!(a / b, symbol_table, waiting_map, a, b)?,
+            Expression::Remainder(a, b) => op!(a % b, symbol_table, waiting_map, a, b)?,
+            Expression::Add(a, b) => op!(a + b, symbol_table, waiting_map, a, b)?,
+            Expression::Sub(a, b) => op!(a - b, symbol_table, waiting_map, a, b)?,
+            Expression::Shl(a, b) => op!(a << b, symbol_table, waiting_map, a, b)?,
+            Expression::Shr(a, b) => op!(a >> b, symbol_table, waiting_map, a, b)?,
+            Expression::Ashr(a, b) => op!(a.ashr(b), symbol_table, waiting_map, a, b)?,
+            Expression::And(a, b) => op!(a & b, symbol_table, waiting_map, a, b)?,
+            Expression::Or(a, b) => op!(a | b, symbol_table, waiting_map, a, b)?,
+            Expression::Xor(a, b) => op!(a ^ b, symbol_table, waiting_map, a, b)?,
         };
 
         Ok(EvalReturn {
@@ -252,4 +72,17 @@ impl Spanned<&Expression> {
             waiting_map,
         })
     }
+}
+
+pub fn get_operand(
+    val: &Spanned<Expression>,
+    symbol_table: &SymbolTable,
+    waiting_map: &mut HashMap<Intern<std::string::String>, Span>,
+) -> Result<Spanned<ExpressionResult>, Error> {
+    let EvalReturn {
+        result,
+        waiting_map: sub_waiting_map,
+    } = (*val).as_ref().eval(symbol_table)?;
+    waiting_map.extend(sub_waiting_map.iter());
+    Ok(val.span_to(result))
 }
