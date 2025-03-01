85b0b9dbb64b2e123581eb57ee46c498a3b67875
Andrew Blommestyn <71096624+ablomm@users.noreply.github.com>
2025-03-01 00:38:35 -0500

    Put back verilator as default simulator

--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-simulator_path="$(dirname "$BASH_SOURCE")/../cpu/build/icarus/Vsimulator"
+simulator_path="$(dirname "$BASH_SOURCE")/../cpu/build/verilator/Vsimulator"
 ./$simulator_path "$@"
